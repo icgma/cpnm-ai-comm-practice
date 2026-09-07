b32427b67a0cd33dfc21851a044adef6d324cd1c
whoisa <whoisa@users.noreply.github.com>
2026-09-07 16:01:55 +0800

    feat: 门户接入百度统计并统一文档页羊皮纸

---
+++ analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function () {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?959465d4f30700d97fc79ce460247967";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
